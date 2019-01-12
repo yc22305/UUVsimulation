53d3676ca85d9ce5cf9e50ebce8e3f2b0f2260af
yc22305 <fir22305@gmail.com>
2019-01-11 19:30:39 -0500

    add LQR controller; example nominal speed is set to 5

--- AnalysisLinearSys.m
+++ AnalysisLinearSys.m
@@ -7,7 +7,7 @@ N_states = 4;
 N_inputs = 2;
 
 %% Fixed u. Varying Z
-nominal_u = 2;
+nominal_u = 5;
 N = 100;
 Zk_start = 0.5; Zk_final = 5;
 Zk = linspace(Zk_start,Zk_final,N);
@@ -33,30 +33,39 @@ set(dcm_obj,'UpdateFcn',@myCursor);
 
 %% Fixed u. Varying Z. (see the change with three different u)
 nominal_u = [2 5 7];
-cmaps = {cmap1, cmap2, cmap3};
+% cmaps = {cmap1, cmap2, cmap3};
 N = 100;
 Zk_start = 0.5; Zk_final = 5;
 Zk = linspace(Zk_start,Zk_final,N);
 p_real = zeros(3,N);
+p_imag = zeros(3,N);
 
 fhandle = figure;
 for j = 1:3
-    current_cmap = cmaps{j};
-    color_idx = 1;
+%     current_cmap = cmaps{j};
+%     color_idx = 1;
     for i = 1:N
         nominal_x0 = [0; Zk(i); nominal_u(j); 0]; % matters to X
         nominal_data_setting = [Zk(i); nominal_u(j)]; % Set Z, u.
         [A,B,nominal_x,nominal_input] = getLinearSys(nominal_x0,nominal_data_setting);
         r = eig(A(2:end,2:end));
         
-        if (color_idx > size(current_cmap,1))
-            color_idx = 1;
+%         if (color_idx > size(current_cmap,1))
+%             color_idx = 1;
+%         end
+%         plot3(real(r),imag(r),Zk(i)*ones(size(r,1),1),'Color',current_cmap(color_idx,:),'Marker','.','LineStyle','none'), hold on;
+%         color_idx = color_idx+1;
+        switch j
+            case 1 
+                plot3(real(r),imag(r),Zk(i)*ones(size(r,1),1),'b','Marker','.','MarkerSize',8,'LineStyle','none'), hold on;
+            case 2
+                plot3(real(r),imag(r),Zk(i)*ones(size(r,1),1),'r','Marker','.','MarkerSize',8,'LineStyle','none'), hold on;
+            case 3 
+                plot3(real(r),imag(r),Zk(i)*ones(size(r,1),1),'g','Marker','.','MarkerSize',8,'LineStyle','none'), hold on;
         end
-        plot3(real(r),imag(r),Zk(i)*ones(size(r,1),1),'Color',current_cmap(color_idx,:),'Marker','.','LineStyle','none'), hold on;
-        color_idx = color_idx+1;
-        
-        compr = r(imag(r)~=0);
-        p_real(j,i) = real(compr(1)); % pick one eigenvalue of the conjugate pair
+        conjidx = r(imag(r)~=0);
+        p_real(j,i) = real(conjidx(1)); % pick one eigenvalue of the conjugate pair
+        p_imag(j,i) = imag(conjidx(1)); % pick one eigenvalue of the conjugate pair
     end
 end
 view(0,90);
@@ -68,6 +77,12 @@ plot(Zk,p_real(1,:),'LineWidth', 2); hold on,
 plot(Zk,p_real(2,:),'r','LineWidth', 2); hold on,
 plot(Zk,p_real(3,:),'g','LineWidth', 2);
 title('Real part of the conjugate eigenvalues (fixed u)'), xlabel('depth (Z)') ,ylabel('Real part');
+legend(['u = ',num2str(nominal_u(1))],['u = ',num2str(nominal_u(2))],['u = ',num2str(nominal_u(3))]);
+figure,
+plot(Zk,p_imag(1,:),'LineWidth', 2); hold on,
+plot(Zk,p_imag(2,:),'r','LineWidth', 2); hold on,
+plot(Zk,p_imag(3,:),'g','LineWidth', 2);
+title('Imaginary part of the conjugate eigenvalues (fixed u)'), xlabel('depth (Z)') ,ylabel('Imaginary part');
 legend(['u = ',num2str(nominal_u(1))],['u = ',num2str(nominal_u(2))],['u = ',num2str(nominal_u(3))]);
 
 %% Fixed Z. Varying u
@@ -96,8 +111,8 @@ dcm_obj = datacursormode(fhandle);
 set(dcm_obj,'UpdateFcn',@myCursor);
 
 %% Fixed Z. Varying u. (see the change with three different Z)
-nominal_Z = [0.5 1 1.5];
-cmaps = {cmap1, cmap2, cmap3};
+nominal_Z = [0.5 1.5 2.5];
+% cmaps = {cmap1, cmap2, cmap3};
 N = 100;
 uk_start = 1; uk_final = 10;
 uk = linspace(uk_start,uk_final,N);
@@ -105,22 +120,30 @@ p_real = zeros(3,N);
 
 fhandle = figure;
 for j = 1:3
-    current_cmap = cmaps{j};
-    color_idx = 1;
+%     current_cmap = cmaps{j};
+%     color_idx = 1;
     for i = 1:N
         nominal_x0 = [0; nominal_Z(j); uk(i); 0]; % matters to X
         nominal_data_setting = [nominal_Z(j); uk(i)]; % Set Z, u.
         [A,B,nominal_x,nominal_input] = getLinearSys(nominal_x0,nominal_data_setting);
         r = eig(A(2:end,2:end));
         
-        if (color_idx > size(current_cmap,1))
-            color_idx = 1;
+%         if (color_idx > size(current_cmap,1))
+%             color_idx = 1;
+%         end
+%         plot3(real(r),imag(r),uk(i)*ones(size(r,1),1),'Color',current_cmap(color_idx,:),'Marker','.','LineStyle','none'), hold on;
+%         color_idx = color_idx+1;
+        switch j
+            case 1 
+                plot3(real(r),imag(r),uk(i)*ones(size(r,1),1),'b','Marker','.','MarkerSize',8,'LineStyle','none'), hold on;
+            case 2
+                plot3(real(r),imag(r),uk(i)*ones(size(r,1),1),'r','Marker','.','MarkerSize',8,'LineStyle','none'), hold on;
+            case 3 
+                plot3(real(r),imag(r),uk(i)*ones(size(r,1),1),'g','Marker','.','MarkerSize',8,'LineStyle','none'), hold on;
         end
-        plot3(real(r),imag(r),uk(i)*ones(size(r,1),1),'Color',current_cmap(color_idx,:),'Marker','.','LineStyle','none'), hold on;
-        color_idx = color_idx+1;
-        
-        compr = r(imag(r)~=0);
-        p_real(j,i) = real(compr(1)); % pick one eigenvalue of the conjugate pair
+        conjidx = r(imag(r)~=0);
+        p_real(j,i) = real(conjidx(1)); % pick one eigenvalue of the conjugate pair
+        p_imag(j,i) = imag(conjidx(1)); % pick one eigenvalue of the conjugate pair
     end
 end
 view(0,90);
@@ -132,6 +155,12 @@ plot(uk,p_real(1,:),'LineWidth', 2); hold on,
 plot(uk,p_real(2,:),'r','LineWidth', 2); hold on,
 plot(uk,p_real(3,:),'g','LineWidth', 2);
 title('Real part of the conjugate eigenvalues (fixed Z)'), xlabel('surge speed (u)') ,ylabel('Real part');
+legend(['Z = ',num2str(nominal_Z(1))],['Z = ',num2str(nominal_Z(2))],['Z = ',num2str(nominal_Z(3))]);
+figure,
+plot(uk,p_imag(1,:),'LineWidth', 2); hold on,
+plot(uk,p_imag(2,:),'r','LineWidth', 2); hold on,
+plot(uk,p_imag(3,:),'g','LineWidth', 2);
+title('Imaginary part of the conjugate eigenvalues (fixed Z)'), xlabel('surge speed (u)') ,ylabel('Imaginary part');
 legend(['Z = ',num2str(nominal_Z(1))],['Z = ',num2str(nominal_Z(2))],['Z = ',num2str(nominal_Z(3))]);
 
 %% PID controller design (Classical controller design--focus on SISO)
@@ -166,7 +195,7 @@ dDetMB33_over_dZ = -3157*Z^5 + 11770*Z^4 - 17430*Z^3 + 12920*Z^2 - 4853*Z + 757.
 A_coe = m+MB11_plus_DetMB11;
 B_coe = dDetMB11_over_dZ;
 C_coe = dDetMB13_over_dZ;
-D_coe = m+MB31_plus_DetMB31;
+D_coe = MB31_plus_DetMB31;
 E_coe = m+MB33_plue_DetMB33;
 F_coe = -0.5*dDetMB11_over_dZ;
 G_coe = 0.5*(dDetMB31_over_dZ-dDetMB13_over_dZ);
@@ -194,7 +223,7 @@ G22b = [c*i 0];
 % % Here we consider two types of modes: first-order and second-order mode.
 % % Note that for our system, a pole is always zero (e.g. Type-1 system), independent of depth and surge speed; this implies that a integration controller is not necessary.
 % % Note that kG11,... are always empty because the system is physically realizable.
-% nominal_u_setting = 2;
+% nominal_u_setting = 5;
 % nominal_Z_setting = 0.2:0.02:5;
 % N =length(nominal_Z_setting);
 % for idx = 1:N
@@ -214,16 +243,16 @@ G22b = [c*i 0];
 %     
 %     figure(1), % weight
 %     h11 = plot(nominal_Z_setting(idx),2*abs(rG11(imag(rG11)~=0)),'b.'); hold on,
-%     h12 = plot(nominal_Z_setting(idx),rG11(imag(rG11)==0),'r.'); hold on,
+%     h12 = plot(nominal_Z_setting(idx),abs(rG11(imag(rG11)==0)),'r.'); hold on,
 %     figure(2),
 %     h21 = plot(nominal_Z_setting(idx),2*abs(rG12(imag(rG12)~=0)),'b.'); hold on,
-%     h22 = plot(nominal_Z_setting(idx),rG12(imag(rG12)==0),'r.'); hold on,
+%     h22 = plot(nominal_Z_setting(idx),abs(rG12(imag(rG12)==0)),'r.'); hold on,
 %     figure(3),
 %     h31 = plot(nominal_Z_setting(idx),2*abs(rG21(imag(rG21)~=0)),'b.'); hold on,
-%     h32 = plot(nominal_Z_setting(idx),rG21(imag(rG21)==0),'r.'); hold on,
+%     h32 = plot(nominal_Z_setting(idx),abs(rG21(imag(rG21)==0)),'r.'); hold on,
 %     figure(4),
 %     h41 = plot(nominal_Z_setting(idx),2*abs(rG22(imag(rG22)~=0)),'b.'); hold on,
-%     h42 = plot(nominal_Z_setting(idx),rG22(imag(rG22)==0),'r.'); hold on,
+%     h42 = plot(nominal_Z_setting(idx),abs(rG22(imag(rG22)==0)),'r.'); hold on,
 % end
 % figure(1), title('G11 Weight'), ylabel('Weight'), xlabel('Z'), legend([h11(1) h12(1)],'second-order term','first-order term');
 % figure(2), title('G12 Weight'), ylabel('Weight'), xlabel('Z'), legend([h21(1) h22(1)],'second-order term','first-order term');
@@ -250,16 +279,16 @@ G22b = [c*i 0];
 % 
 %     figure(5), % weight
 %     h61 = plot(nominal_u_setting(idx),2*abs(rG11(imag(rG11)~=0)),'b.'); hold on,
-%     h62 = plot(nominal_u_setting(idx),rG11(imag(rG11)==0),'r.'); hold on,
+%     h62 = plot(nominal_u_setting(idx),abs(rG11(imag(rG11)==0)),'r.'); hold on,
 %     figure(6),
 %     h71 = plot(nominal_u_setting(idx),2*abs(rG12(imag(rG12)~=0)),'b.'); hold on,
-%     h72 = plot(nominal_u_setting(idx),rG12(imag(rG12)==0),'r.'); hold on,
+%     h72 = plot(nominal_u_setting(idx),abs(rG12(imag(rG12)==0)),'r.'); hold on,
 %     figure(7),
 %     h81 = plot(nominal_u_setting(idx),2*abs(rG21(imag(rG21)~=0)),'b.'); hold on,
-%     h82 = plot(nominal_u_setting(idx),rG21(imag(rG21)==0),'r.'); hold on,
+%     h82 = plot(nominal_u_setting(idx),abs(rG21(imag(rG21)==0)),'r.'); hold on,
 %     figure(8),
 %     h91 = plot(nominal_u_setting(idx),2*abs(rG22(imag(rG22)~=0)),'b.'); hold on,
-%     h92 = plot(nominal_u_setting(idx),rG22(imag(rG22)==0),'r.'); hold on,
+%     h92 = plot(nominal_u_setting(idx),abs(rG22(imag(rG22)==0)),'r.'); hold on,
 % end
 % figure(5), title('G11 Weight'), ylabel('Weight'), xlabel('u'), legend([h61(1) h62(1)],'second-order term','first-order term');
 % figure(6), title('G12 Weight'), ylabel('Weight'), xlabel('u'), legend([h71(1) h72(1)],'second-order term','first-order term');
@@ -270,7 +299,7 @@ G22b = [c*i 0];
 %%%%%%%%% Specific nominal states %%%%%%%%%
 % From the plot of modes' weight, valid nominal states are chosen to design
 % the controller.
-nominal_data_setting = [3; 2]; % Set Z, u.
+nominal_data_setting = [0.5; 2]; % Set Z, u.
 nominal_x0 = [0; nominal_data_setting(1); nominal_data_setting(2); 0]; % nominal initial state: X, Z, u, w
 [A,B,nominal_x,nominal_input] = getLinearSys(nominal_x0,nominal_data_setting);
 
@@ -329,7 +358,7 @@ G12fbsys = feedback((Kpw+Kdw*tf('s'))*tf(G12b_subs,a_subs),1);
 [rG12fb,pG12fb,kG12fb] = residue(G12fb_b,G12fb_a)
 
 % control gain matrix
-K = [0 Kpu 0; Kpw 0 Kdw]
+K = [0 Kpu 0; 0 0 0]
 % Check eigenvalues
 lambda = eig(A(2:end,2:end)-B(2:end,:)*K)
 
@@ -347,4 +376,21 @@ p = [-1/timeConst; -sigma+1i*Frequency; -sigma-1i*Frequency];
 K = place(A(2:end,2:end),B(2:end,:),p)
 [eigVector,lambda] = eig(A(2:end,2:end)-B(2:end,:)*K)
 
+%% LQR
+nominal_data_setting = [0.5; 2]; % Set Z, u.
+nominal_x0 = [0; nominal_data_setting(1); nominal_data_setting(2); 0]; % nominal initial state: X, Z, u, w
+[A,B,nominal_x,nominal_input] = getLinearSys(nominal_x0,nominal_data_setting);
 
+DevPercentage = 10;
+x0 = nominal_x0*(1+0.01*DevPercentage); % initial state: X, Z, u, w
+
+% values used to non-dimensionalize Q, R and states and inputs, depending on the overshoot which is set to limited in 10%
+Zbound = abs(nominal_x(2)-x0(2))*1.1; 
+ubound = abs(nominal_x(3)-x0(3))*1.1;
+
+Q = eye(3)*10;
+Q(1,1) = Q(1,1)/Zbound^2; Q(3,3) = Q(3,3)/Zbound^2;
+Q(2,2) = Q(2,2)/ubound^2;
+R = eye(2);
+
+[K,S,lambda] = lqr(A(2:end,2:end),B(2:end,:),Q,R)
